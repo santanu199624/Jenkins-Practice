5463cdbddb953a425a98ef9e544e7ce2e02b27bc
santanu199624 <24maitysantanu@gmail.com>
2026-01-18 06:19:28 +0530

    sec change

--- src/test/java/Package1/Amazon_Browser_Launch.java
+++ src/test/java/Package1/Amazon_Browser_Launch.java
@@ -11,7 +11,7 @@ public class Amazon_Browser_Launch {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
-		System.out.println("Browser luanch successful!");
+		System.out.println("Browser luanch successfully!");
 		driver.quit();
 	}
 
